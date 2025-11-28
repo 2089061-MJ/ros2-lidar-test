20e90b65d3edd2d62de089416d319cf282c06ec5
2089061-MJ <2089061@pcu.ac.kr>
2025-11-28 09:42:14 +0900

    8 commit

--- lidar_package/lidar_package/lidar_test.py
+++ lidar_package/lidar_package/lidar_test.py
@@ -3,7 +3,6 @@ from rclpy.node import Node
 import math
 import random
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
 
 ANGLE_MIN_DEG = 0
 ANGLE_MAX_DEG = 359
@@ -32,16 +31,14 @@ def make_the_wall(ranges, center_deg, width_deg):
     half_width = width_deg // 2
     for offset in range(-half_width, half_width + 1):
         idx = (center_deg + offset) % NUM_POINTS
-        ranges[idx] = 0.4
+        ranges[idx] = 0.4   # 벽 거리
 
 
 def pattern_front_wall(scan):
     make_the_wall(scan.ranges, center_deg=0, width_deg=40)
 
-
 def pattern_left_wall(scan):
     make_the_wall(scan.ranges, center_deg=90, width_deg=30)
-
 
 def pattern_right_wall(scan):
     make_the_wall(scan.ranges, center_deg=270, width_deg=30)
@@ -60,6 +57,7 @@ def generate_single_scan(pattern_name):
 
 AVAILABLE_PATTERNS = ["front_wall", "left_wall", "right_wall"]
 
+
 class LidarPublisher(Node):
     def __init__(self):
         super().__init__('lidar_mock_publisher')
@@ -67,49 +65,19 @@ class LidarPublisher(Node):
         # LaserScan 퍼블리셔
         self.publisher = self.create_publisher(LaserScan, 'lidar_scan', 10)
 
-        # turtle의 cmd_vel 토픽을 발행
-        self.cmd_pub = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
-
-        # 2초마다 발행하는 타이머
         self.timer = self.create_timer(2.0, self.timer_callback)
-
         self.get_logger().info("Lidar Publisher Started")
 
     def timer_callback(self):
         pattern = random.choice(AVAILABLE_PATTERNS)
         scan_msg = generate_single_scan(pattern)
 
-        # LaserScan 메시지 타임스탬프 추가
         scan_msg.header.stamp = self.get_clock().now().to_msg()
         scan_msg.header.frame_id = "lidar"
 
         self.publisher.publish(scan_msg)
-        self.get_logger().info(f"Published Lidar Pattern: {pattern}")
+        self.get_logger().info(f"Published Pattern: {pattern}")
 
-        cmd = self.turtle_action(pattern)
-        self.cmd_pub.publish(cmd)
-        self.get_logger().info(f"linear={cmd.linear.x:.2f}, angular={cmd.angular.z:.2f}")
-
-    # turtle1의 방향 제어
-    def turtle_action(self, pattern):
-        cmd = Twist()
-
-        # 앞에 벽이 있을 때 거북이를 멈춤
-        if pattern == "front_wall":
-            cmd.linear.x = 0.0
-            cmd.angular.z = 0.0
-
-        # 왼쪽에 벽이 있을 때 거북이를 오른쪽으로 이동
-        elif pattern == "left_wall":
-            cmd.linear.x = 0.2
-            cmd.angular.z = -0.7
-
-        # 오른쪽에 벽이 있을 때 거북이를 왼쪽으로 이동
-        elif pattern == "right_wall":
-            cmd.linear.x = 0.2
-            cmd.angular.z = 0.7
-
-        return cmd
 
 def main(args=None):
     rclpy.init(args=args)
